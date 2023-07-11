1de016a33dc213a1750e6b682ce3a489b6f8c7c6
sadrul <sadrul@neocon.com>
2023-07-11 10:39:25 +0600

    CU-85ztfyd64 - NTW Api with Backend Service

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\dashboard\ServiceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Service
+    Route::prefix('service')->group(function () {
+        Route::get('/create', [ServiceController::class, 'create'])->name('service.create');
+        Route::post('/store', [ServiceController::class, 'store'])->name('service.store');
+        Route::get('/index', [ServiceController::class, 'index'])->name('service.index');
+        Route::get('/edit/{id}', [ServiceController::class, 'edit'])->name('service.edit');
+        Route::put('/{id}', [ServiceController::class, 'update'])->name('service.update');
+        Route::delete('/delete/{id}', [ServiceController::class, 'destroy'])->name('service.destroy');
+    });
 });
 
 require __DIR__ . '/auth.php';
